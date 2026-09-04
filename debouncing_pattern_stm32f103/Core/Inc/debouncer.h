6054983981c920261a4fc716ee4939fa0ac04be5
bytecod3 <emwiti658@gmail.com>
2026-09-04 15:46:25 +0300

    ADDS debouncing pattern

---
+++ debouncing_pattern_stm32f103/Core/Inc/debouncer.h
@@ -0,0 +1,16 @@
+/*
+ * debouncer.h
+ *
+ *  Created on: Sep 3, 2026
+ *      Author: edwin.mwiti
+ */
+
+#ifndef INC_DEBOUNCER_H_
+#define INC_DEBOUNCER_H_
+
+#include "stm32f1xx.h"
+
+void debounce_delay(int delay_time);
+
+
+#endif /* INC_DEBOUNCER_H_ */
---
+++ debouncing_pattern_stm32f103/Core/Src/debouncer.c
@@ -0,0 +1,15 @@
+/*
+ * debouncer.c
+ *
+ *  Created on: Sep 3, 2026
+ *      Author: edwin.mwiti
+ */
+
+#include "debouncer.h"
+
+/// implements a non-blocking delay
+void debounce_delay(int delay_time) {
+
+}
+
+
